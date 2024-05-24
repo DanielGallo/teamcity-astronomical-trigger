67ac9ba2ee07adb0d77c36c640966f1f5fe1ae19
danielgallo <danielgallo@TeamCity>
2024-05-24 14:56:37 +0000

    TeamCity change in 'TeamCity Astronomical Trigger' project: Versioned settings configuration updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -24,7 +24,7 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 'Debug' option is available in the context menu for the task.
 */
 
-version = "2022.04"
+version = "2024.03"
 
 project {
 
